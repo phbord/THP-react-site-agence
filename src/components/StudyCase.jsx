e20f5446a8184e83dec12579cc642f5f32afd127
Septentrion50 <erwan56lecorre@gmail.com>
2021-08-11 12:30:44 +0200

    Links to clients

--- src/components/StudyCase.jsx
+++ src/components/StudyCase.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import clients from '../assets/db/clients';
 
 const StudyCase = () => {
+  const { clientSlug } = useParams();
+  const [currentClient, setCurrentClient] = useState(undefined);
+
+  useEffect(() => {
+    const foundClient = clients.find((client) => client.slug === clientSlug);
+    setCurrentClient(foundClient);
+  }, [clientSlug]);
+
   return (
     <div className="my-5">
-      <h2></h2>
-      <p></p>
+      {currentClient ? (
+        <>
+          <h2>{currentClient.name}</h2>
+          <h3>{currentClient.title}</h3>
+          <p>{currentClient.body}</p>
+        </>
+      ) : (
+        <p>Pas de client trouvé</p>
+      )}
     </div>
   );
 };
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import StudyCase from 'components/StudyCase';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const Works = () => {
 	return (
 	  <div className="container my-5">
       <h1 className="mb-4">Au fil des années, nous avons pu accompagner les meilleurs</h1>
       <p>Découvrez pas à pas comment nous avons été présent pour lancer vos marques préférées.</p>
+      <Router>
+        <nav className="d-flex">
+          <Link to="/works/platon-study-case" className="nav-link me-2 ps-0">Platon</Link>
+          <Link to="/works/solane-study-case" className="nav-link me-2">Solane</Link>
+          <Link to="/works/sedal-study-case" className="nav-link me-2">Sedal</Link>
+        </nav>
+        <Switch>
+          <Route path="/works/:clientSlug">
+            <StudyCase />
+          </Route>
+        </Switch>
+      </Router>
     </div>
 	);
 };
